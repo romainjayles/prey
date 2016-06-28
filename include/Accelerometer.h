e390dd828bdc7fb0ceed054e7d718697de067d4f
Romain Jayles <romain.jayles@ensimag.grenoble-inp.fr>
2016-06-28 21:29:22 +0200

    Adding Accelerometer thread

--- include/Accelerometer.h
+++ include/Accelerometer.h
@@ -5,4 +5,58 @@
 #ifndef PREY_ACCELEROMETER_H
 #define PREY_ACCELEROMETER_H
 
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+class Accelerometer{
+protected:
+
+    /**
+     * The last value acquired
+     */
+    double x_acceleration_value;
+    double y_acceleration_value;
+    double z_acceleration_value;
+
+
+    int update_frequency_ms; //The refresh time of accelerometer values
+
+    /**
+     * The interface to the real implementation
+     */
+    virtual int _init_accelerometer_sensor() = 0;
+
+    virtual double _get_x_acceleration() = 0;
+
+    virtual double _get_y_acceleration() = 0;
+
+    virtual double _get_z_acceleration() = 0;
+
+    thread accelerometer_task;
+    static mutex accelerometer_lock;
+
+public:
+    Accelerometer();
+
+    /**
+     * The copy method (used for threading purpose)
+     */
+
+
+    /**
+     * Init the accelerometer class
+     */
+    int init(int update_frequency_ms);
+
+    int teardown();
+
+    int get_current_values(double *x_acceleration_value, double *y_acceleration_value, double *z_acceleration_value);
+
+    void _main_task();
+
+
+};
+
 #endif //PREY_ACCELEROMETER_H
---
+++ source/Accelerometer.cpp
@@ -0,0 +1,54 @@
+//
+// Created by jaylesr on 28/06/16.
+//
+
+#include <Accelerometer.h>
+#include <unistd.h>
+#include <thread>
+#include <mutex>
+
+
+//Initialization of the static variable
+mutex Accelerometer::accelerometer_lock;
+
+Accelerometer::Accelerometer() {
+    x_acceleration_value = 0.0;
+    y_acceleration_value = 0.0;
+    z_acceleration_value = 0.0;
+    update_frequency_ms = 0;
+}
+
+/**
+ * Init the accelerometer class
+ */
+int Accelerometer::init(int update_frequency_ms) {
+    this->update_frequency_ms = update_frequency_ms;
+    accelerometer_task = thread(&Accelerometer::_main_task, this);
+}
+
+int Accelerometer::teardown() {
+    //stop
+}
+
+int Accelerometer::get_current_values(double *x_acceleration_value, double *y_acceleration_value, double *z_acceleration_value) {
+    //mutex accelerometer_lock;
+    accelerometer_lock.lock();
+    *x_acceleration_value = this->x_acceleration_value;
+    *y_acceleration_value = this->y_acceleration_value;
+    *z_acceleration_value = this->z_acceleration_value;
+    accelerometer_lock.unlock();
+}
+
+void Accelerometer::_main_task() {
+    while (true) {
+        printf("Accel\n");
+        accelerometer_lock.lock();
+        x_acceleration_value = _get_x_acceleration();
+        y_acceleration_value = _get_y_acceleration();
+        z_acceleration_value = _get_z_acceleration();
+        accelerometer_lock.unlock();
+        usleep(update_frequency_ms * 1000);
+    }
+}
+
+
