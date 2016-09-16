8bc4b3d953bda2a5166be11c519bfeed56926b02
Romain Jayles <romain.jayles@ensimag.grenoble-inp.fr>
2016-09-16 17:30:03 +0200

    adding flight_controller

---
+++ include/Flight_controller.h
@@ -0,0 +1,81 @@
+/**
+ * This class will handle the correct orientation of the uav
+ * It receive order from other class and send command to the motors to reach the desired goal
+ *
+ */
+
+#ifndef _FLIGHT_CONTROLLER_
+#define _FLIGHT_CONTROLLER_
+
+#include <Logger.h>
+#include <Orientation_manager.h>
+#include <thread>
+
+class Flight_controller{
+
+private:
+
+    float desired_roll;
+    float desired_pitch;
+    float desired_yaw;
+
+    int roll_precision;
+    int pitch_precision;
+    int yaw_precision;
+
+    Logger &logger;
+    Orientation_manager orientation_manager;
+
+    /**
+     * The update frequency of the main thread
+     */
+    int update_frequency_ms;
+
+    static mutex flight_controller_lock;
+    thread flight_controller_task;
+    bool run;
+
+public:
+
+    Flight_controller(Logger &logger);
+
+    /**
+     * To init the flight controller
+     */
+    int init(int update_frequency_ms);
+
+    /**
+     * Allow to teardown the flight controller
+     */
+    int teardown();
+
+    /**
+    * Allow to command the pitch
+    * desired_pitch : the desired angle
+    * precision : the input - output tolerance
+    */
+    int command_pitch(float desired_pitch, int precision);
+
+    /**
+    * Allow to command the roll
+    * desired_roll : the desired angle
+    * precision : the input - output tolerance
+    */
+    int command_roll(float desired_roll, int precision);
+
+
+    /**
+    * Allow to command the yaw
+    * desired_yaw : the desired angle
+    * precision : the input - output tolerance
+    */
+    int command_yaw(float desired_yaw, int precision);
+
+    /**
+     * The main task of the class, will be threaded
+     */
+    void _main_task();
+
+};
+
+#endif
---
+++ source/Flight_controller.cpp
@@ -0,0 +1,94 @@
+#include <Flight_controller.h>
+#include <Logger.h>
+#include <unistd.h>
+
+//Initialization of the static variable
+mutex Flight_controller::flight_controller_lock;
+
+Flight_controller::Flight_controller(Logger &logger):logger(logger), orientation_manager(logger){
+    desired_pitch = 0.0;
+    desired_yaw = 0.0;
+    desired_roll = 0.0;
+
+    roll_precision = 0;
+    pitch_precision = 0;
+    yaw_precision = 0;
+}
+
+/**
+ * To init the flight controller
+ */
+int Flight_controller::init(int update_frequency_ms){
+    this->update_frequency_ms = update_frequency_ms;
+
+    // Launching the task necessary for flight_controller
+    logger.log(LOG_INFO, "Launching orientation manager");
+    orientation_manager.init(ORIENTATION_MANAGER_UPDATE_FREQUENCY_MS);
+
+    // Launching the flight controllerN
+    run = true;
+    flight_controller_task = thread(&Flight_controller::_main_task, this);
+    return 0;
+}
+
+/**
+ * Allow to teardown the flight controller
+ */
+int Flight_controller::teardown(){
+    orientation_manager.teardown();
+
+    run = false;
+    flight_controller_task.join();
+
+    logger.log(LOG_INFO, "Flight Controller task terminated");
+    return 0;
+}
+
+/**
+* Allow to command the pitch
+* desired_pitch : the desired angle
+* precision : the input - output tolerance
+*/
+int Flight_controller::command_pitch(float desired_pitch, int precision){
+    flight_controller_lock.lock();
+    this->desired_pitch = desired_pitch;
+    this->pitch_precision = precision;
+    flight_controller_lock.unlock();
+}
+
+/**
+* Allow to command the roll
+* desired_roll : the desired angle
+* precision : the input - output tolerance
+*/
+int Flight_controller::command_roll(float desired_roll, int precision){
+    flight_controller_lock.lock();
+    this->desired_roll = desired_roll;
+    this->roll_precision = precision;
+    flight_controller_lock.unlock();
+}
+
+
+/**
+* Allow to command the yaw
+* desired_yaw : the desired angle
+* precision : the input - output tolerance
+*/
+int Flight_controller::command_yaw(float desired_yaw, int precision){
+    flight_controller_lock.lock();
+    this->desired_yaw = desired_yaw;
+    this->yaw_precision = precision;
+    flight_controller_lock.unlock();
+}
+
+
+void Flight_controller::_main_task(){
+
+    while(run){
+        logger.log(LOG_ERROR, "Do nothing");
+        flight_controller_lock.lock();
+        flight_controller_lock.unlock();
+        usleep(update_frequency_ms*1000);
+    }
+
+}
