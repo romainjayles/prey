a8d613a08d92801916b4b0942ffec7b5d4f50bb4
Romain Jayles <romain.jayles@ensimag.grenoble-inp.fr>
2016-09-16 22:49:57 +0200

    Allow remote execution of 3D_visualisation.py

--- script/3D_visualisation.py
+++ script/3D_visualisation.py
@@ -6,6 +6,15 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+LOCAL_COMMAND = ["./../source/orientation_visualisation"]
+REMOTE_COMMAND = ["ssh" ,"-t" ,"pi@raspberrypi.local", "./prey/build/source/orientation_visualisation"]
+
+def print_help():
+    print("Tool used to draw a 3D representation of the UAV")
+    print("The first argument must be an integer representing the desired update value of both gyroscopes and accelerometers")
+    print("The second argument is -l to launch the script with pre-recorded values or -r to launch on the raspberry-pi via ssh")
+    print("Example : python 3D_visualisation.py 50 -r")
+
 nodes = (
     (1, -0.2, -1),
     (1, 0.2, -1),
@@ -32,7 +41,18 @@ connections = (
     (5,7)
 )
 
-frequency = int(sys.argv[1])
+try:
+    frequency = int(sys.argv[1])
+    if sys.argv[2]== '-r':
+        print "Remote launch"
+        COMMAND_LINE = REMOTE_COMMAND
+    elif sys.argv[2]== '-l':
+        print "Local launch"
+        COMMAND_LINE = LOCAL_COMMAND
+except:
+    print_help()
+    exit()
+
 lines_buffer = []
 actual_roll = 0
 actual_pitch = 0
@@ -41,7 +61,7 @@ actual_yaw = 0
 print "Launching the 3D representation of the position of the device"
 print " * Update frequency set at " + str(frequency) + "ms"
 print " * Warning experiment show that python is too slow for processing at frequencies bellow 30ms"
-proc = subprocess.Popen(["./../source/orientation_visualisation", str(frequency)], stdout=subprocess.PIPE)
+proc = subprocess.Popen(COMMAND_LINE + [str(frequency)], stdout=subprocess.PIPE)
 
 
 def update_values():
